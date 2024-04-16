7036a8facb6941d9481e4c2fa153b5939a0a3381
kmh5038 <kmh5038@gmail.com>
2024-04-16 10:38:53 +0900

    remove

--- TCA_Tutorial/Contact/AddContactView.swift
+++ TCA_Tutorial/Contact/AddContactView.swift
@@ -8,25 +8,3 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct AddContactView: View {
-  @Bindable var store: StoreOf<AddContactFeature>
-
-  var body: some View {
-    Form {
-      TextField("Name", text: $store.contact.name.sending(\.setName))
-      Button("Save") {
-        store.send(.saveButtonTapped)
-      }
-    }
-    .toolbar {
-      ToolbarItem {
-        Button("Cancel") {
-          store.send(.cancelButtonTapped)
-        }
-      }
-    }
-  }
-}
-
-
-
--- TCA_Tutorial/Contact/ContactDetailView.swift
+++ TCA_Tutorial/Contact/ContactDetailView.swift
@@ -7,16 +7,3 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct ContactDetailView: View {
-    @Bindable var store: StoreOf<ContactDetailFeature>
-    
-    var body: some View {
-        Form {
-            Button("Delete") {
-                store.send(.deleteButtonTapped)
-            }
-        }
-        .navigationBarTitle(Text(store.contact.name))
-        .alert($store.scope(state: \.alert, action: \.alert))
-    }
-}
